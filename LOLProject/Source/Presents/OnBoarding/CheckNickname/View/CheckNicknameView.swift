ff17a2712829621692472c69f3cd96068381e717
cody <qudgus1984@naver.com>
2023-01-30 17:24:29 +0900

    [Feat] CheckNinameView UI 작업

--- LOLProject/Source/Presents/OnBoarding/CheckNickname/View/CheckNicknameView.swift
+++ LOLProject/Source/Presents/OnBoarding/CheckNickname/View/CheckNicknameView.swift
@@ -10,12 +10,32 @@ import SnapKit
 
 class CheckNicknameView: BaseView {
     
+    private var bgView: UIView = {
+       let view = UIView()
+        view.backgroundColor = .green1
+        return view
+    }()
     
+    private var nicknameTextfield: UITextField = {
+       let textfield = UITextField()
+        textfield.placeholder = "소환사 이름을 입력해주세요."
+        return textfield
+    }()
+    
+    private var searchButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("검색", for: .normal)
+        return button
+    }()
     
     override func configureUI() {
-        
+        [bgView, nicknameTextfield, searchButton].forEach {
+            addSubview($0)
+        }
     }
     override func setConstraints() {
-        
+        bgView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
